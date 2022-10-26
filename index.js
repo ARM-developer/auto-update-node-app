dc0e6692f73281b1cbea35a9b96cc4b660d62992
ARM-developer <LE17081486@merida.tecnm.mx>
2022-10-26 17:25:57 -0500

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 let express = require('express');
 let bodyParser = require('body-parser');
-let AutoUpdater = require('auto-updater');
-
-let autoupdater = new AutoUpdater({
- pathToJson: '',
- autoupdate: false,
- checkgit: true,
- jsonhost: 'raw.githubusercontent.com',
- contenthost: 'codeload.github.com',
- progressDebounce: 0,
- devmode: false
-});
-
 
 let app = express();
 let port = process.env.PORT || 3525;
+
+const { downloadRelease } = require('@terascope/fetch-github-release');
+
+const user = 'some user';
+const repo = 'some repo';
+const outputdir = 'some output directory';
+const leaveZipped = false;
+const disableLogging = false;
+
 
 // Convierte una petición recibida (POST-GET...) a objeto JSON
 app.use(bodyParser.urlencoded({extended:false}));
@@ -28,50 +25,34 @@ app.get('/', function(req, res){
 
 app.listen(port, function(){
 	console.log(`Server running in http://localhost:${port}`);
-});
 
-// State the events
-autoupdater.on('git-clone', function() {
-	console.log("You have a clone of the repository. Use 'git pull' to be up-to-date");
-});
-autoupdater.on('check.up-to-date', function(v) {
-	console.info("You have the latest version: " + v);
-});
-autoupdater.on('check.out-dated', function(v_old, v) {
-	console.warn("Your version is outdated. " + v_old + " of " + v);
-	autoupdater.fire('download-update'); // If autoupdate: false, you'll have to do this manually.
-	// Maybe ask if the'd like to download the update.
-});
-autoupdater.on('update.downloaded', function() {
-	console.log("Update downloaded and ready for install");
-	autoupdater.fire('extract'); // If autoupdate: false, you'll have to do this manually.
-});
-autoupdater.on('update.not-installed', function() {
-	console.log("The Update was already in your folder! It's read for install");
-	autoupdater.fire('extract'); // If autoupdate: false, you'll have to do this manually.
-});
-autoupdater.on('update.extracted', function() {
-	console.log("Update extracted successfully!");
-	console.warn("RESTART THE APP!");
-});
-autoupdater.on('download.start', function(name) {
-	console.log("Starting downloading: " + name);
-});
-autoupdater.on('download.progress', function(name, perc) {
-	process.stdout.write("Downloading " + perc + "% \033[0G");
-});
-autoupdater.on('download.end', function(name) {
-	console.log("Downloaded " + name);
-});
-autoupdater.on('download.error', function(err) {
-	console.error("Error when downloading: " + err);
-});
-autoupdater.on('end', function() {
-	console.log("The app is ready to function");
-});
-autoupdater.on('error', function(name, e) {
-	console.error(name, e);
-});
+	const { downloadRelease } = require('@terascope/fetch-github-release');
 
-// Start checking
-autoupdater.fire('check');
+	const user = 'ARM-developer';
+	const repo = 'auto-update-node-app';
+	const outputdir = './';
+	const leaveZipped = false;
+	const disableLogging = false;
+
+	// Define a function to filter releases.
+	function filterRelease(release) {
+		// Filter out prereleases.
+		console.log(release);
+		return release.prerelease === false;
+	}
+
+	// Define a function to filter assets.
+	function filterAsset(asset) {
+		console.log(asset);
+		// Select assets that contain the string 'windows'.
+		return asset.name.includes('windows');
+	}
+
+	downloadRelease(user, repo, outputdir, filterRelease, filterAsset, leaveZipped, disableLogging)
+	.then(function() {
+		console.log('All done!');
+	})
+	.catch(function(err) {
+		console.error(err.message);
+	});
+});
